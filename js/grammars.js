78d2977755cec28b2d85594ae5b9367b66673142
bosque <88783969+newo3210@users.noreply.github.com>
2024-09-13 23:00:33 -0300

    Add files via upload

---
+++ js/grammars.js
@@ -0,0 +1,17 @@
+
+
+var grammars = {
+
+
+    e : { "origin":["#emotional#, #machine#"],
+    "emotional": ["suave", "llorarias", "tu no eres esto", "serias enorme"],
+    "machine": ["presiona una tecla", "exit", "erase", "repeat", "go away"]
+
+
+
+
+
+
+}
+
+}
